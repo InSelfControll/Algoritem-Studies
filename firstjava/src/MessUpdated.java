fc7ad09661182ae6bbb66e884fed40e87dd09138
InSelfControl <admin@hostme.co.il>
2020-12-25 01:19:22 +0200

    Updated

--- firstjava/src/MessUpdated.java
+++ firstjava/src/MessUpdated.java
@@ -5,20 +5,27 @@ public class MessUpdated {
 
 	public static void main(String[] args) {
 
+		// int UserSelection = us();
 		Scanner userinput = new Scanner(System.in);
 		Random rnd = new Random();
 
 		int[] usernumber = new int[6];
 		int[] randomcounter = new int[6];
 
-		int i,j = 0;
+		int i,j,l;
 		int counters = 0;
-		randomcounter[j] = rnd.nextInt(37) + 1;
+		
+		for (j = 0; j < 6 ;j ++) {
+			randomcounter[j] = rnd.nextInt(37) + 1;
+			/*System.out.println("Random numbers are: " + randomcounter[j]);*/
+		}
+		
 		for (i = 0; i < 6; i++) {
 			usernumber[i] = userinput.nextInt();
 			for (j = 0; j < 6; j++) {
 				if (usernumber[i] == randomcounter[j]) {
 					counters++;
+				}else {
 				}
 			}
 		}
