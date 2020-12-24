2d34b48d81c0af1ea8f9bd33a6de0a357088ba52
InSelfControl <admin@hostme.co.il>
2020-12-24 20:36:13 +0200

    Updated

--- firstjava/src/MessUpdated.java
+++ firstjava/src/MessUpdated.java
@@ -1,55 +1,33 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class MessUpdated {
 
 	public static void main(String[] args) {
 
 		// int UserSelection = us();
+		@SuppressWarnings("resource")
 		Scanner userinput = new Scanner(System.in);
 		Random rnd = new Random();
 
-		// User Selection
-		int unumber = userinput.nextInt();
-		int unumber1 = userinput.nextInt();
-		int unumber2 = userinput.nextInt();
-		int unumber3 = userinput.nextInt();
-		int unumber4 = userinput.nextInt();
-		int unumber5 = userinput.nextInt();
+		int[] usernumber = new int[5];
+		int[] randomcounter = new int[5];
 
-		System.out.println("\n");
-		System.out.println(
-				unumber + " " + unumber1 + " " + unumber2 + " " + unumber3 + " " + unumber4 + " " + unumber5 + "");
-
-		int counter = rnd.nextInt(37) + 1;
-		int counter1 = rnd.nextInt(37) + 1;
-		int counter2 = rnd.nextInt(37) + 1;
-		int counter3 = rnd.nextInt(37) + 1;
-		int counter4 = rnd.nextInt(37) + 1;
-		int counter5 = rnd.nextInt(37) + 1;
-
+		int i;
 		int counters = 0;
-		int i = 0;
-		if (unumber == counter || unumber1 == counter || unumber2 == counter || unumber3 == counter
-				|| unumber4 == counter || unumber5 == counter && unumber == counter1 || unumber1 == counter1
-				|| unumber2 == counter1 || unumber3 == counter1 || unumber4 == counter1
-				|| unumber5 == counter1 && unumber == counter2 || unumber1 == counter2 || unumber2 == counter2
-				|| unumber3 == counter2 || unumber4 == counter2 || unumber5 == counter2 && unumber == counter3
-				|| unumber1 == counter3 || unumber2 == counter3 || unumber3 == counter3 || unumber4 == counter3
-				|| unumber5 == counter3 && unumber == counter4 || unumber1 == counter4 || unumber2 == counter4
-				|| unumber3 == counter4 || unumber4 == counter4 || unumber5 == counter4 && unumber == counter5
-				|| unumber1 == counter5 || unumber2 == counter5 || unumber3 == counter5 || unumber4 == counter5
-				|| unumber5 == counter5) {
-			counters++;
-			System.out.println(counter + " " + counter1 + " " + counter2 + " " + counter3 + " " + counter4 + " "
-					+ counter5 + "\n");
-			return;
 
-		} else {
-			System.out.println("You failed! ");
-			System.out.println(
-					counter + " " + counter1 + " " + counter2 + " " + counter3 + " " + counter4 + " " + counter5 + "\n");
+		for (i = 1; i <= 6; i++) {
+			usernumber[i] = userinput.nextInt();
+			randomcounter[i] = rnd.nextInt(37) + 1;
+			if (usernumber == randomcounter) {
+				System.out.println(usernumber[i] + "You correct! ");
+				counters++;
+			} else {
+				System.out.println("You're failed! " + randomcounter[i]);
+			}
+			System.out.println(randomcounter[i]);
+			System.out.println("You're correct in: " + counters);
 		}
-		System.out.println("You guessed  " + counters + " right!" + " \n");
 	}
 }
