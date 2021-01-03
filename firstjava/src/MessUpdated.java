ff99643c228d76148c108c85d0410b6cdab8bc0d
InSelfControl <admin@hostme.co.il>
2021-01-03 23:40:01 +0200

    0

--- firstjava/src/MessUpdated.java
+++ firstjava/src/MessUpdated.java
@@ -9,7 +9,8 @@ public class MessUpdated {
 		lotto();
 	}
 
-	public static boolean lotto() {
+	@SuppressWarnings("unlikely-arg-type")
+	public static int lotto() {
 
 		// int UserSelection = us();
 		Scanner userinput = new Scanner(System.in);
@@ -34,26 +35,17 @@ public class MessUpdated {
 			randomcounter[j] = rnd.nextInt(37) + 1;
 			// Print the random numbers for testing if there is something twice
 			System.out.println("Random numbers are: " + randomcounter[j]);
-			if (randomcounter[j] == randomcounter[j]) {
-				continue;
-			}
-			if (randomcounter[j] == 0) {
-				continue;
+			while (isSame && j < randomcounter.length) {
+				if (randomcounter.equals(randomcounter)) {
+					randomcounter[j]++;
+					//isSame =  true;
+				}
+				if (randomcounter.equals(0) ) {
+					randomcounter[j]++;
+					continue;
+				}
 			}
 		}
-		/*
-		 * for (int j1 : randomcounter) { if (!rnd1.add(j1)) { System.out.println(j1 +
-		 * " is a duplicate"); } }
-		 */
-		// isSame = false;
-		// isSame = false;
-		// while (isSame) {
-		// isSame = false;
-		/*
-		 * if (randomcounter[j] == randomcounter[j]) { continue; } if (randomcounter[j]
-		 * == 0) { break; }
-		 */ // }
-		// }
 
 		for (i = 0; i < 6; i++) {
 			usernumber[i] = userinput.nextInt();
@@ -72,6 +64,6 @@ public class MessUpdated {
 		for (j = 0; j < 6; j++) {
 			System.out.println("Random numbers are: " + randomcounter[j]);
 		}
-		return false;
+		return 0;
 	}
 }
