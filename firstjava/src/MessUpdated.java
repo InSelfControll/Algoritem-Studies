5a02e099e0397174f190fe4a8cbe282ccd8686f6
InSelfControl <admin@hostme.co.il>
2021-01-04 01:20:01 +0200

    0

--- firstjava/src/MessUpdated.java
+++ firstjava/src/MessUpdated.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
@@ -8,7 +10,7 @@ public class MessUpdated {
 	public static void main(String[] args) {
 		lotto();
 	}
-	
+
 	public static int lotto() {
 
 		// int UserSelection = us();
@@ -16,20 +18,24 @@ public class MessUpdated {
 		Random rnd = new Random();
 
 		int[] usernumber = new int[6];
-		int[] randomcounter = new int[6];
-		 Set<Integer> numbers = new HashSet<>();
+		// int[] randomcounter = new int[6];
+		List<Integer> randomcounter = new ArrayList<Integer>();
 
 		int i = 0;
 		int j = 0;
 		int counters = 0;
 
-		randomcounter[j] = rnd.nextInt(37)+1;
-		contains(randomcounter, j);
-		
+		while (randomcounter.size() < 6) {
+			int number = rnd.nextInt(37) + 1;
+			if (!randomcounter.contains(number)) {
+				randomcounter.add(number);
+			}
+		}
+
 		for (i = 0; i < 6; i++) {
 			usernumber[i] = userinput.nextInt();
 			for (j = 0; j < 6; j++) {
-				if (usernumber[i] == randomcounter[j]) {
+				if (usernumber[i] == randomcounter.get(i)) {
 					counters++;
 				} else {
 				}
@@ -41,16 +47,9 @@ public class MessUpdated {
 		System.out.println("You're correct in: " + counters);
 		System.out.println("\n");
 		for (j = 0; j < 6; j++) {
-			System.out.println("Random numbers are: " + randomcounter[j]);
+			System.out.println("Random numbers are: " + randomcounter.get(j));
 		}
 		return 0;
 	}
-	public static boolean contains(int[] randomcounter, int x){
-        for(int i = 0; i < randomcounter.length; i++){
-            if(randomcounter[i] == x){
-                return true;
-            }
-        }
-        return false;
-    }
+
 }
