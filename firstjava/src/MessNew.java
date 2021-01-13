839b7884954cd635a0da31fba312bc5dbcd884a0
InSelfControl <admin@hostme.co.il>
2021-01-13 12:00:01 +0200

    0

--- firstjava/src/MessNew.java
+++ firstjava/src/MessNew.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
--- firstjava/src/MessUpdated.java
+++ firstjava/src/MessUpdated.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Set;
 
 public class MessUpdated {
 
