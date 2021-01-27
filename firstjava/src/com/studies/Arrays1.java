56c0298f6f64423a167364afb78373cddd0b48c0
InSelfControl <admin@hostme.co.il>
2021-01-27 16:15:01 +0200

    0

--- firstjava/src/com/studies/Arrays1.java
+++ firstjava/src/com/studies/Arrays1.java
@@ -12,8 +12,10 @@ public class Arrays1 {
 		int[] I1 = new int[10];
 		int[] Z1 = new int[20];
 
-		I1[10] = rand.nextInt(10);
-		Z1[20] = rand.nextInt(20);
+		int i = 0;
+		int z = 0;
+		I1[i] = rand.nextInt(10);
+		Z1[z] = rand.nextInt(20);
 
 		int[] I = ForZ(I1);
 		int[] Z = ForI(Z1);
@@ -29,7 +31,7 @@ public class Arrays1 {
 		int i;
 		System.out.println("First Array! ");
 		for (i = 0; i < I1.length; i++) {
-			I1[10] = rand.nextInt(10) + 1;
+			I1[i] = rand.nextInt(10) + 1;
 			System.out.println(I1[i]);
 		}
 		return I1;
@@ -45,7 +47,7 @@ public class Arrays1 {
 		System.out.println("\n");
 		System.out.println("Second Array! ");
 		for (z = 0; z < Z1.length; z++) {
-			Z1[20] = rand.nextInt(20);
+			Z1[z] = rand.nextInt(20);
 			System.out.println(Z1[z]);
 		}
 		return Z1;
